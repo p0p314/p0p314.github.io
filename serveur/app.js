0f176ec97e630037dd3a138c6baa346f3e1e8934
Cédric <86252008+popille@users.noreply.github.com>
2025-07-11 11:57:48 +0200

    ajout compteur fav

--- serveur/app.js
+++ serveur/app.js
@@ -8,12 +8,31 @@ const fs = require("fs");
 const { json } = require('stream/consumers');
 
 const jsonData = JSON.parse(fs.readFileSync('./data/rare_words.json', 'utf-8'));
+const jsonFav = JSON.parse(fs.readFileSync('./data/fav.json', 'utf-8'));
 
 app.use(express.json())
 app.use(cors())
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'vues')); // dossier des templates
+
+const favPath = path.join(__dirname, 'data', 'fav.json');
+
+// Fonction pour charger le JSON de favoris en toute sécurité
+function loadFavs() {
+  try {
+    const content = fs.readFileSync(favPath, 'utf8').trim();
+    if (!content) return {};
+    return JSON.parse(content);
+  } catch {
+    return {};
+  }
+}
+
+// Fonction pour sauvegarder le JSON de favoris
+function saveFavs(favs) {
+  fs.writeFileSync(favPath, JSON.stringify(favs, null, 2), 'utf8');
+}
 
 
 app.get("/random", (req,res) => {
@@ -38,4 +57,42 @@ app.get('/mots/:word', (req, res) => {
     res.render('mots', { mot }); // passe l'objet à la vue
 });
 
+app.post('/favoris/mots', (req, res) => {
+    const { action, mot } = req.body;
+  
+    if (action !== 'ajouter' && action !== 'retirer') {
+      return res.status(400).json({ error: "Action non reconnue" });
+    }
+    if (!mot || typeof mot !== 'string') {
+      return res.status(400).json({ error: "Le paramètre 'mot' est requis et doit être une chaîne" });
+    }
+  
+    try {
+      const favoris = loadFavs();
+  
+    if(action == 'ajouter'){
+
+        if (favoris[mot]) {
+            favoris[mot] += 1;
+        } else {
+            favoris[mot] = 1;
+        }
+    } else if(action == 'retirer'){
+        if (favoris[mot] && favoris[mot] > 0 ) {
+            favoris[mot] -= 1;
+        }
+    }
+  
+      saveFavs(favoris);
+  
+      res.json({
+        message: "Mot ajouté avec succès",
+        mot: mot,
+        count: favoris[mot],
+      });
+    } catch (error) {
+      console.error('Erreur lors de l\'ajout du mot:', error);
+      res.status(500).json({ error: "Erreur interne du serveur" });
+    }
+  });
 module.exports = app;
